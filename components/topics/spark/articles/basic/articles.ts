0b8f8419b2b1459e665bd6529595029f8d89494a
dxu <dean.xu@asml.com>
2021-06-29 16:36:04 +0800

    fix error

--- components/topics/spark/articles/basic/articles.ts
+++ components/topics/spark/articles/basic/articles.ts
@@ -2,7 +2,7 @@ import A0, {meta as m0} from './0-overview.mdx'
 import A1, {meta as m1} from './1-hello-world.mdx'
 import A2, {meta as m2} from './2-SparkSession.mdx'
 
-import {ArticleData, ArticleSet} from "../article";
+import {ArticleData, ArticleSet} from "../../../topic";
 
 const Overview = {Component: A0, meta: m0}
 const HelloWorld = {Component: A1, meta: m1}
