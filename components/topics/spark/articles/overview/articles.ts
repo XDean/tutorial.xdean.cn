a7797cb4250a522ea049680fbf28e63e174906c9
dxu <dean.xu@asml.com>
2021-07-01 11:04:13 +0800

    article mapreduce

--- components/topics/spark/articles/overview/articles.ts
+++ components/topics/spark/articles/overview/articles.ts
@@ -1,17 +1,20 @@
 import A0, {meta as m0} from './0-overview.mdx'
 import A1, {meta as m1} from './1-background.mdx'
 import A2, {meta as m2} from './2-tech.mdx'
+import A3, {meta as m3} from './3-mapreduce.mdx'
 
 import {ArticleData, ArticleSet} from "../../../topic";
 
 const Overview = {Component: A0, meta: m0}
 const Background = {Component: A1, meta: m1}
 const Tech = {Component: A2, meta: m2}
+const MapReduce = {Component: A3, meta: m3}
 
 export const OverviewArticles: ArticleData[] = [
   Overview,
   Background,
   Tech,
+  MapReduce,
 ]
 
 export const OverviewArticleSet: ArticleSet = {
--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withMDX = require('@next/mdx');
 const rehypePrism = require('@mapbox/rehype-prism');
+const remarkMath = require('remark-math')
+const rehypeKatex = require('rehype-katex')
 
 module.exports = withMDX({
   extension: /\.(md|mdx)$/,
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [remarkMath],
     rehypePlugins: [
+      rehypeKatex,
       [rehypePrism, {
         ignoreMissing: true
       }]
