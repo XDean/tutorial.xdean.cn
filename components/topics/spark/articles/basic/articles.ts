45da3c611b113b155835004e61dd23f22105858e
dxu <dean.xu@asml.com>
2021-07-16 09:49:22 +0800

    article spark rdd-closure

--- components/topics/spark/articles/basic/articles.ts
+++ components/topics/spark/articles/basic/articles.ts
@@ -1,7 +1,8 @@
 import A0, {meta as m0} from './0-overview.mdx'
 import A1, {meta as m1} from './1-hello-world.mdx'
 import A2, {meta as m2} from './2-rdd-dataframe.mdx'
-import A3, {meta as m3} from './3-rdd.mdx'
+import A3, {meta as m3} from './3-rdd-basic.mdx'
+import A4, {meta as m4} from './4-rdd-closure.mdx'
 
 import {ArticleData, ArticleSet} from "../../../topic";
 
@@ -10,6 +11,7 @@ export const BasicArticles: ArticleData[] = [
   {Component: A1, meta: m1},
   {Component: A2, meta: m2},
   {Component: A3, meta: m3},
+  {Component: A4, meta: m4},
 ]
 
 export const BasicArticleSet: ArticleSet = {
