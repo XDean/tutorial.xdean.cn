43180b680c177642df06c85d60b01d71f6b1b839
dxu <dean.xu@asml.com>
2021-08-30 17:31:36 +0800

    [react] [project] add 1 article

--- components/topics/react/articles/project/articles.ts
+++ components/topics/react/articles/project/articles.ts
@@ -1,11 +1,13 @@
 import A0, {meta as m0} from './0-introduce.mdx'
 import A1, {meta as m1} from './1-basic.mdx'
+import A2, {meta as m2} from './2-create-react-app.mdx'
 import {ArticleData, ArticleSet} from "../../../topic";
 
 
 export const ProjectArticles: ArticleData[] = [
   {Component: A0, meta: m0},
   {Component: A1, meta: m1},
+  {Component: A2, meta: m2},
 ]
 
 export const ProjectArticleSet: ArticleSet = {
