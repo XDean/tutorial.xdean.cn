00fa248f5850ca9d33c82aaab17cd1fc967cdbd9
dxu <dean.xu@asml.com>
2021-09-01 09:19:53 +0800

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,13 @@ import {AppProps} from "next/dist/pages/_app";
 import '../styles/global.css'
 import Head from 'next/head'
 import {useLocaleString} from "../components/util/locale";
+import {useGA} from "../common/util/ga";
+
+const GA_ID = 'G-1LNJZ286FW'
 
 function MyApp({Component, pageProps}: AppProps) {
   const localString = useLocaleString()
+  useGA(GA_ID)
   return (
     <>
       <Head>
