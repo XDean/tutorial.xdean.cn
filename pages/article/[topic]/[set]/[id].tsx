a1ebbe23aeeef2b7413d101219cb3eef612d193b
dxu <dean.xu@asml.com>
2022-04-06 19:43:46 +0800

    add static path for sitemap

--- pages/article/[topic]/[set]/[id].tsx
+++ pages/article/[topic]/[set]/[id].tsx
@@ -1,75 +1,90 @@
-import {DefaultLayout} from "../../../../components/layout/DefaultLayout";
-import {Router, useRouter} from 'next/router'
-import {TOC} from "../../../../components/TOC";
-import {useEffect, useRef, useState} from "react";
-import {Article} from "../../../../components/Article";
-import Head from 'next/head'
-import {AllTopics} from "../../../../components/topics/topics";
-import {ArticleData, ArticleLocale, ArticleSet, getLocaleArticleSets, Topic} from "../../../../components/topics/topic";
-import clsx from "clsx";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
-import {useLocale} from "../../../../components/util/hooks";
-import {useLocaleString} from "../../../../components/util/locale";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { Router, useRouter } from 'next/router';
+import { FC, useEffect, useRef, useState } from 'react';
+import { Article } from '../../../../components/Article';
+import { DefaultLayout } from '../../../../components/layout/DefaultLayout';
+import { TOC } from '../../../../components/TOC';
+import {
+  ArticleData,
+  ArticleLocale,
+  ArticleSet,
+  getLocaleArticleSets,
+  Topic,
+} from '../../../../components/topics/topic';
+import { AllTopics } from '../../../../components/topics/topics';
+import { useLocale } from '../../../../components/util/hooks';
+import { useLocaleString } from '../../../../components/util/locale';
 
-export default function Index() {
-  const router = useRouter()
-  const locale = useLocale()
-  const localString = useLocaleString()
-  const [topic, setTopic] = useState<Topic>()
-  const [localArticleSets, setLocaleArticleSets] = useState<ArticleLocale>()
-  const [articleSet, setArticleSet] = useState<ArticleSet>()
-  const [article, setArticle] = useState<ArticleData>()
-  const [openToc, setOpenToc] = useState(false)
-  const articleContainerRef = useRef<HTMLDivElement>(null)
+type Params = {
+  topic: string
+  set: string
+  id: string
+}
+
+type Props = Params
+
+const Index: FC<Props> = () => {
+  const router = useRouter();
+  const locale = useLocale();
+  const localString = useLocaleString();
+  const [topic, setTopic] = useState<Topic>();
+  const [localArticleSets, setLocaleArticleSets] = useState<ArticleLocale>();
+  const [articleSet, setArticleSet] = useState<ArticleSet>();
+  const [article, setArticle] = useState<ArticleData>();
+  const [openToc, setOpenToc] = useState(false);
+  const articleContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!router.isReady) {
-      return
+      return;
     }
-    const {topic: topicId, set, id} = router.query
-    const topic = AllTopics.find(e => e.id == topicId)
+    const {topic: topicId, set, id} = router.query;
+    const topic = AllTopics.find(e => e.id == topicId);
     if (!topic) {
-      router.replace(`/`)
-      return
+      router.replace(`/`);
+      return;
     }
-    const localeArticleSets = getLocaleArticleSets(topic, locale)
-    const articleSet = localeArticleSets.sets.find(e => e.id === set)
-    const article = articleSet?.articles.find(e => e.meta.id === id)
+    const localeArticleSets = getLocaleArticleSets(topic, locale);
+    const articleSet = localeArticleSets.sets.find(e => e.id === set);
+    const article = articleSet?.articles.find(e => e.meta.id === id);
 
     if (!!article) {
-      setTopic(topic)
-      setLocaleArticleSets(localeArticleSets)
-      setArticleSet(articleSet)
-      setArticle(article)
+      setTopic(topic);
+      setLocaleArticleSets(localeArticleSets);
+      setArticleSet(articleSet);
+      setArticle(article);
     } else {
-      router.replace(`/article/${topic.id}`)
+      router.replace(`/article/${topic.id}`);
     }
-  }, [router])
+  }, [router]);
 
   useEffect(() => {
     const handler = () => {
       articleContainerRef.current?.scroll({
         top: 0,
         left: 0,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     };
     Router.events.on('routeChangeComplete', handler);
-    return () => Router.events.off('routeChangeComplete', handler)
-  }, [article])
+    return () => Router.events.off('routeChangeComplete', handler);
+  }, [article]);
 
   if (!router.isReady || !topic || !localArticleSets || !articleSet || !article) {
-    return null
+    return null;
   }
 
-  const topicTitle = topic.title || localString.get("topicTitle", topic.name)
+  const topicTitle = topic.title || localString.get('topicTitle', topic.name);
 
   return (
     <DefaultLayout topbar={{
       title: topicTitle,
       left: <topic.Icon/>,
-      locales: topic.articles.map(e => e.locale || 'zh')
+      locales: topic.articles.map(e => e.locale || 'zh'),
     }}>
       <Head>
         <title>{article.meta.name} | {topicTitle}</title>
@@ -94,5 +109,27 @@ export default function Index() {
         </div>
       </div>
     </DefaultLayout>
-  )
-}
+  );
+};
+
+export default Index;
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ctx => {
+  return {
+    props: ctx.params!,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  return {
+    paths: AllTopics.flatMap(t => t.articles.flatMap(as => as.sets.flatMap(s => s.articles.map(a => ({
+      params: {
+        topic: t.id,
+        set: s.id,
+        id: a.meta.id,
+      },
+      locale: as.locale,
+    }))))),
+    fallback: true,
+  };
+};
--- pages/article/[topic]/index.tsx
+++ pages/article/[topic]/index.tsx
@@ -1,25 +1,25 @@
-import {useRouter} from "next/router";
-import {useEffect} from "react";
-import {AllTopics} from "../../../components/topics/topics";
-import {useLocale} from "../../../components/util/hooks";
-import {getLocaleArticleSets} from "../../../components/topics/topic";
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { getLocaleArticleSets } from '../../../components/topics/topic';
+import { AllTopics } from '../../../components/topics/topics';
+import { useLocale } from '../../../components/util/hooks';
 
 export default function Index() {
-  const router = useRouter()
-  const locale = useLocale()
+  const router = useRouter();
+  const locale = useLocale();
   useEffect(() => {
     if (!router.isReady) {
-      return
+      return;
     }
-    const {topic: topicId} = router.query
-    const topic = AllTopics.find(e => e.id == topicId)
+    const {topic: topicId} = router.query;
+    const topic = AllTopics.find(e => e.id == topicId);
     if (!!topic) {
       const sets = getLocaleArticleSets(topic, locale).sets;
-      const first = sets[0]
-      router.replace(`/article/${topic.id}/${first.id}/${first.articles[0].meta.id}`)
+      const first = sets[0];
+      router.replace(`/article/${topic.id}/${first.id}/${first.articles[0].meta.id}`);
     } else {
-      router.replace('/')
+      router.replace('/');
     }
-  }, [locale, router])
-  return null
+  }, [locale, router]);
+  return null;
 }
